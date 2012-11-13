f493ea8c15b1510a5f13c447bea0fb3c7b441970
Andreas Berre <andreas.berre@gmail.com>
2012-11-13 22:23:59 +0100

    service for adding new entries

--- src/main/java/bera/phonebook/Phonebook.java
+++ src/main/java/bera/phonebook/Phonebook.java
@@ -1,7 +1,9 @@
 package bera.phonebook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -10,13 +12,22 @@ import java.util.List;
 @Controller
 @RequestMapping("/phonebook/")
 public class Phonebook {
+    private static List<Entry> entries = new ArrayList<Entry>();
+    static {
+        entries.add(new Entry("Andreas", 98008257));
+        entries.add(new Entry("Anna", 95752390));
+    }
 
     @RequestMapping("getEntries")
     @ResponseBody
     public List<Entry> getEntries(){
-        List<Entry> entries = new ArrayList<Entry>();
-        entries.add(new Entry("Andreas", 98008257));
-        entries.add(new Entry("Anna", 95752390));
         return entries;
+    }
+
+    @RequestMapping(value="newEntry", method= RequestMethod.POST)
+    @ResponseBody
+    public String newEntry(@RequestBody Entry newEntry){
+        entries.add(newEntry);
+        return "OK";
     }
 }
